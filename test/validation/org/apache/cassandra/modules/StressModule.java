08577836fe67819afa77ba63054326d2a631b2e0
Philip Thompson <ptnapoleon@gmail.com>
2015-06-17 14:23:22 -0400

    Should not define executor in abstract module

--- test/validation/org/apache/cassandra/modules/StressModule.java
+++ test/validation/org/apache/cassandra/modules/StressModule.java
@@ -32,7 +32,6 @@ public abstract class StressModule extends Module
     public StressModule(Config config, StressSettings settings)
     {
         super(config);
-        executor = new DebuggableThreadPoolExecutor("Stress", Thread.NORM_PRIORITY);
         this.settings = settings;
     }
 
