d26be762bbfd185b38930376244f393468f06b38
sylvestor88 <sylvestor.george@sjsu.edu>
2015-06-18 14:12:58 -0700

    Node Logs Capture
    Capture Node logs for a test and archive old logs
    
    Capture Node Logs and zip older logs if any
    
    Capture Node Logs and zip older logs if any
    
    Capture Node Logs and zip older logs if any
    
    Capture Node Logs and zip older logs if any
    
    Capture Node Logs and zip older logs if any

--- test/validation/org/apache/cassandra/bridges/ccmbridge/CCMBridge.java
+++ test/validation/org/apache/cassandra/bridges/ccmbridge/CCMBridge.java
@@ -17,27 +17,23 @@ package org.apache.cassandra.bridges.ccmbridge;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import com.google.common.io.Files;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.apache.cassandra.bridges.ArchiveClusterLogs;
 import org.apache.cassandra.bridges.Bridge;
 import org.apache.cassandra.htest.Config;
-
-import org.apache.commons.io.FileUtils;
 
 public class CCMBridge extends Bridge
 {
 
-    private int testNodeCount;
+    private int nodeCount;
     static final File CASSANDRA_DIR = new File("./");
 
     private final Runtime runtime = Runtime.getRuntime();
@@ -56,19 +52,14 @@ public class CCMBridge extends Bridge
 
     public CCMBridge(int nodeCount)
     {
-        testNodeCount = nodeCount;
+        this.nodeCount = nodeCount;
         this.ccmDir = Files.createTempDir();
         execute("ccm create %s -n %d --install-dir %s", DEFAULT_CLUSTER_NAME, nodeCount, CASSANDRA_DIR);
     }
 
-    public void captureLogs(String testName)
-    {
-        stop();
-        preserveClusterLogs(testName);
-    }
-
     public void destroy()
     {
+        stop();
         execute("ccm remove");
     }
 
@@ -191,20 +182,19 @@ public class CCMBridge extends Bridge
         }
     }
 
-    public void preserveClusterLogs(String testName){
-
-        int count = 1;
+    public void captureLogs(String testName)
+    {
         String clusterLogsPath = ccmDir + "/" + DEFAULT_CLUSTER_NAME;
         String folderName = testName;
         String existingFolder = CASSANDRA_DIR + "/build/test/logs/validation/" + folderName;
 
-        if(checkForFolder(existingFolder))
+        if(ArchiveClusterLogs.checkForFolder(existingFolder))
         {
-            zipExistingDirectory(existingFolder);
+            ArchiveClusterLogs.zipExistingDirectory(existingFolder);
         }
 
-        while (count <= testNodeCount){
-
+        for(int count = 1; count <= nodeCount; count++)
+        {
             File sourceFile = new File(clusterLogsPath + "/node" + count + "/logs" + "/system.log");
 
             File destFile = new File(CASSANDRA_DIR + "/build/test/logs/validation/" + folderName + "/node" + count + ".log");
@@ -217,75 +207,6 @@ public class CCMBridge extends Bridge
             {
                 e.printStackTrace();
             }
-
-            count++;
-        }
-
-    }
-
-    public boolean checkForFolder(String dirPath){
-
-        File file = new File(dirPath);
-        return file.exists();
-    }
-
-    public void zipExistingDirectory(String existDir){
-
-        String sourceFolderName =  existDir;
-        long unixTime = System.currentTimeMillis() / 1000L;
-        String outputFileName = existDir + "_" + unixTime + ".zip";
-
-        try
-        {
-            FileOutputStream fos = new FileOutputStream(outputFileName);
-            ZipOutputStream zos = new ZipOutputStream(fos);
-            zos.setLevel(9);
-            compressFiles(zos, sourceFolderName, sourceFolderName);
-            zos.close();
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void compressFiles(ZipOutputStream zos,String folderName,String baseFolderName){
-
-        File f = new File(folderName);
-
-        try
-        {
-            if (f.exists())
-            {
-
-                if (f.isDirectory())
-                {
-                    File f2[] = f.listFiles();
-                    for (int i = 0; i < f2.length; i++)
-                    {
-                        compressFiles(zos, f2[i].getAbsolutePath(), baseFolderName);
-                    }
-                }
-                else
-                {
-                    String entryName = folderName.substring(baseFolderName.length() + 1, folderName.length());
-                    ZipEntry ze = new ZipEntry(entryName);
-                    zos.putNextEntry(ze);
-                    FileInputStream in = new FileInputStream(folderName);
-                    int len;
-                    byte buffer[] = new byte[1024];
-                    while ((len = in.read(buffer)) > 0)
-                    {
-                        zos.write(buffer, 0, len);
-                    }
-                    in.close();
-                    zos.closeEntry();
-                }
-            }
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
         }
     }
 }
